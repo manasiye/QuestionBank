b816a9043c6041ff577085697b0f6e52f9fc5039
MANASI YERUNKAR <manasi.yerunkar@gmail.com>
2018-03-27 17:14:37 -0400

    Regular Expression Matching
    Implement regular expression matching with support for '.' and '*'.

--- RegularExpressionMatching.java
+++ RegularExpressionMatching.java
@@ -3,39 +3,44 @@ import java.util.*;
 
 public class RegularExpressionMatching {
 
-    public static boolean isMatch(String text, String pattern) {
-        char[] txt = text.toCharArray();
-        char[] pat = pattern.toCharArray();
-        boolean[][] result = new boolean[txt.length + 1][pat.length + 1];
+	public static void main(String[] args) {
+		System.out.println(isMatch("Manasi", "Manasi"));
+		System.out.println(isMatch("Manas", "Manasi*a*b*"));
+		System.out.println(isMatch("", "a*b*"));
+		System.out.println(isMatch("abbbbccc", "a*ab*bbbbc*"));
+		System.out.println(isMatch("abbbbccc", "aa*bbb*bbbc*"));
+		System.out.println(isMatch("abbbbccc", ".*bcc"));
+		System.out.println(isMatch("abbbbccc", ".*bcc*"));
+		System.out.println(isMatch("abbbbccc", ".*bcc*"));
+		System.out.println(isMatch("aaa", "ab*a*c*a"));
+	}
 
-        result[0][0] = true;
+	public static boolean isMatch(String txt, String pat) {
+		char[] text = txt.toCharArray();
+		char[] pattern = pat.toCharArray();
+		boolean[][] T = new boolean[text.length + 1][pattern.length + 1];
 
-        //
-        for (int i = 1; i < result[0].length; i++) {
-            if (pat[i - 1] == '*')
-                result[0][i] = result[0][i - 2];
-        }
+		T[0][0] = true;
 
-        for (int i = 1; i < result.length; i++) {
-            for (int j = 1; j < result[0].length; j++) {
-                if (pat[j - 1] == '.' || pat[j - 1] == txt[j - 1]) {
-                    result[i][j] = result[i - 1][j - 1];
-                } else if (pat[j - 1] == '*') {
-                    result[i][j] = result[i][j - 2];
-                    if (pat[j - 1] == '.' || pat[j - 1] == txt[j - 1]) {
-                        result[i][j] = result[i][j] | result[i - 1][j];
-                    }
-                } else {
-                    result[i][j] = false;
-                }
-            }
-        }
-        return result[txt.length][pat.length];
-    }
+		for (int i = 1; i < T[0].length; i++) {
+			if (pattern[i - 1] == '*')
+				T[0][i] = T[0][i - 2];
+		}
 
-    public static void main(String[] args) {
-        String text = "Manasi";
-        String pattern = ".*";
-        System.out.println(isMatch(text, pattern));
-    }
+		for (int i = 1; i < T.length; i++) {
+			for (int j = 1; j < T[0].length; j++) {
+				if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
+					T[i][j] = T[i - 1][j - 1];
+				} else if (pattern[j - 1] == '*') {
+					T[i][j] = T[i][j - 2];
+					if (pattern[j - 2] == '.' || pattern[j - 2] == text[i - 1]) {
+						T[i][j] = T[i][j] | T[i - 1][j];
+					}
+				} else {
+					T[i][j] = false;
+				}
+			}
+		}
+		return T[text.length][pattern.length];
+	}
 }
